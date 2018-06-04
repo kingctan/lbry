45b640563593ede7d27fc0e691e605f2f2351bc2
hackrush <atulshyan96@gmail.com>
2018-06-04 17:29:44 -0400

    import fixes and removed temporary test function

--- lbrynet/daemon/Daemon.py
+++ lbrynet/daemon/Daemon.py
@@ -797,19 +797,6 @@ class Daemon(AuthJSONRPCServer):
             }
         defer.returnValue(response)
 
-    def jsonrpc_blah(self):
-        """
-        Usage:
-            blah
-
-        Options:
-            None
-
-        Returns:
-            blah bla
-        """
-        return self._render_response(self.component_manager.all_components_running("database", "dht", "wallet"))#""fileManager", "hashAnnouncer", "peerProtocolServer", "reflector", "session", "streamIdentifier", "wallet"))
-
     def jsonrpc_version(self):
         """
         Get lbry version information
